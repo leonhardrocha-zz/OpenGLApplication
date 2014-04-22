bad2b7707171bcb1d3a22d53035d1b98ee88c618
leonhardrocha <leonhardrocha@gmail.com>
2014-04-22 13:17:20 -0300

    Added facetraker  in main loop
    Added facetracker libs;
    Tracker inside Qwidget;

--- OpenGLQtApp/main.cpp
+++ OpenGLQtApp/main.cpp
@@ -1,21 +1,62 @@
 #include "OpenGLQtApp.h"
 #include "AssetWindow.h"
+#include "SingleFace.h"
 
+
+#include <QtQuick/qquickview.h>
 #include <QtWidgets/QApplication>
+//
+//// Program's main entry point
+//DWORD WINAPI StartThread(PVOID lpParam)
+//{
+//	QWidget* parent = static_cast<QWidget*>(lpParam);
+//    SingleFace app(parent, 0);
+//	return app.Run();
+//}
+
 
 int main(int argc, char **argv)
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
+	
     QSurfaceFormat format;
     format.setSamples(4);
-
-    AssetWindow window;
-    window.setFormat(format);
+	AssetWindow window;
+	window.setFormat(format);
     window.resize(640, 480);
-    window.show();
+	window.show();
+
+	QQuickView *view = new QQuickView(&window);
+	QWidget *oglContainer = QWidget::createWindowContainer(view);
+	oglContainer->setMinimumSize(640,480);
+	oglContainer->setMaximumSize(1280,960);
+	oglContainer->setFocusPolicy(Qt::TabFocus);	 
+	
+	QMainWindow mainWindow;
+	mainWindow.menuBar()->addMenu("&File")->addAction("&Exit", &app, SLOT(quit()));
+	QWidget viewWidget(&mainWindow);
+	QVBoxLayout hbox(&viewWidget);
+	hbox.addWidget(oglContainer);
+	
+	QWidget *trackerWidget = new QWidget();
+    mainWindow.setDockNestingEnabled(true);
+	mainWindow.setMenuWidget(trackerWidget);
 
     window.setAnimating(true);
+	/*auto hFaceTrackingThread = CreateThread(NULL, 0, StartThread, (PVOID)trackerWidget, 0, 0);	
+*/
+	SingleFace tracker(trackerWidget, 0);
+	
+	auto requiredSize = mbstowcs( NULL, argv[0], 0) + 1;
+	wchar_t* lpCmdLine = new wchar_t[requiredSize];
+	mbstowcs(lpCmdLine, argv[0], requiredSize);
+	tracker.InitInstanceInHostWindow((PWSTR) lpCmdLine, argc);
+	
+	auto status = app.exec();
 
-    return app.exec();
+	delete oglContainer;
+	delete view;
+	delete trackerWidget;
+    return status;
 }
